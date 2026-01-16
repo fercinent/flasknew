336cdcc8d7974524686a9233efba517260a8a194
Fercinent Thomas <fercinentthomas@gmail.com>
2026-01-16 12:08:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello! My first Flask app 🚀"
+    return render_template("index.html")
 
 if __name__ == "__main__":
+
     app.run(debug=True)
